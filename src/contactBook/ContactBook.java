dfeeb1a9db0e96c56a4d2aa830ea4b3516aa6553
Ana-Maissa-Antunes <ama.antunes@fct.unl.pt>
2022-09-19 17:17:33 +0100

    Updated EP command.

--- src/contactBook/ContactBook.java
+++ src/contactBook/ContactBook.java
@@ -73,14 +73,6 @@ public class ContactBook {
         return result;
     }
 
-    public boolean hasSameNumber(){
-        for(int i=0;i<counter;i++){
-            for(int j = i+1; j< counter;j++)
-                if (contacts[i].getPhone()==contacts[j].getPhone())
-                    return true;}
-        return false;
-    }
-
     private void resize() {
         Contact tmp[] = new Contact[2*contacts.length];
         for (int i=0;i<counter; i++)
@@ -95,6 +87,14 @@ public class ContactBook {
             }
         }
         return null;
+    }
+
+    public boolean hasSamePhoneNum(){
+        for( int i=0;i<counter;i++){
+            for(int j = i+1; j< counter;j++)
+                if (contacts[i].getPhone()==contacts[j].getPhone())
+                    return true;}
+        return false;
     }
 
     public void initializeIterator() {
